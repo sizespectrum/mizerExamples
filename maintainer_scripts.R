3e9eca4d4f67db301b3584a0638625b41e116546
Gustav Delius <gustav.delius@gmail.com>
2020-04-07 14:50:44 +0100

    I should not use `steady()` when updating MizerParams objects because that will change the density dependent reproduction.

--- maintainer_scripts.R
+++ maintainer_scripts.R
@@ -7,9 +7,10 @@ for (file in list.files("data")) {
     name <- load(path)
     print(name)
     if (is(get(name), "MizerParams")) {
-        assign(name, upgradeParams(get(name)))
-        # Uncomment next line if we want to run to new steady state
-        assign(name, steady(get(name)))
+        params <- upgradeParams(get(name))
+        # Uncomment the next two line if we want to run to new steady state
+        # sim <- project(params)
+        # assign(name, setInitialValues(params, sim))
         # We save with version 2 to be backwards compatible to R < 3.5
         save(list = name, file = path, version = 2)
     }
